594c7d2286c247e88f87ea26bad0c3b5bddaec3c
ekul1988 <lukemarut@me.com>
2025-10-31 07:26:34 -0400

    created new layouts

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,10 +1,13 @@
-const ContactPage = () => {
-    return ( <section>
+import type { Route } from "./+types/index";
 
-        <h2 className=" text-3xl font-bold text-white mb-8 text-center">
-            Contact Me
-        </h2>
-    </section> );
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Dev Blog | Welcome" },
+    { name: "Luke Marut's Blog", content: "Welcome to my blog!" },
+  ];
 }
- 
-export default ContactPage;
+
+export default function Contact() {
+   
+  return <section>Contact</section>;
+}
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/index";
-import Hero from "~/components/Hero";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Dev Blog | Welcome" },
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
    
-  return <section><Hero /></section>;
+  return <section>Home</section>;
 }
