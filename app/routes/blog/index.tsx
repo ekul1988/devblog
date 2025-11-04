313eab74a655076873af8b91545c9a0c6ddfc917
ekul1988 <lukemarut@me.com>
2025-11-04 10:31:08 -0500

    added post filter

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,50 +1,70 @@
 import { useState } from "react";
 import Pagination from "~/components/Pagination";
-import type {Route} from "./+types/index";
+import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
 import PostCard from "~/components/PostCard";
-export async function loader ({request}: Route.LoaderArgs): Promise<{posts: PostMeta[]}> {
-    const url = new URL('/posts-meta.json', request.url);
-    const res = await fetch(url.href);
+import PostFilter from "~/components/PostFilter";
 
-    if (!res.ok) throw new Error('Failed to fetch data');
-    
-    const data = await res.json();
-    data.sort((a:PostMeta, b:PostMeta) =>{
-        return new Date(b.date).getTime() - new Date(a.date).getTime();
-    })
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL("/posts-meta.json", request.url);
+  const res = await fetch(url.href);
 
-    return {posts:data};
+  if (!res.ok) throw new Error("Failed to fetch data");
 
+  const data = await res.json();
+  data.sort((a: PostMeta, b: PostMeta) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
 
+  return { posts: data };
 }
 
-const BlogPage = ({loaderData}:Route.ComponentProps) => {
-    const {posts} = loaderData; 
-    const [currentPage, setCurrentPage] = useState(1);
-    const postsPerPage = 3;
-    
-    const totalPages = Math.ceil(posts.length / postsPerPage);
-    const indexOfLast = currentPage * postsPerPage;
-    const indexOfFirst = indexOfLast - postsPerPage;
-    const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 3;
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery.toLocaleLowerCase();
+    return (
+      post.title.toLocaleLowerCase().includes(query) ||
+      post.excerpt.toLocaleLowerCase().includes(query)
+    );
+  });
 
-    return ( <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
-        <h2 className="text-3xl text-white font-bold mb-8">
-            Blog
-        </h2>
-        {currentPosts.map((post) =>(
-        <PostCard post={post} key={post.slug} />
-        ))}
-        {
-            totalPages > 1 && (
-                <Pagination 
-                currentPage={currentPage}
-                totalPages={totalPages}
-                onPageChange={(page) => setCurrentPage(page)} />
-            )
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast);
+
+  return (
+    <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
+      <h2 className="text-3xl text-white font-bold mb-8">Blog</h2>
+      <PostFilter
+        searchQuery={searchQuery}
+        onSearchChange={(query) => {
+          setSearchQuery(query);
+          setCurrentPage(1);
+        }}
+      />{" "}
+      <div className="space-y-8">
+        {currentPosts.length === 0 ? (
+          <p className="text-gray-400 text-center">"Not found"</p>
+        ) : 
+          currentPosts.map((post) => <PostCard post={post} key={post.slug} />)
         }
-    </div> );
-}
- 
+      </div>
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
+    </div>
+  );
+};
+
 export default BlogPage;
