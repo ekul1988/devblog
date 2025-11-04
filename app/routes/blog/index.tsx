f2477913ef725f656e6ec6168bf8b4eb58f2808b
ekul1988 <lukemarut@me.com>
2025-11-04 10:02:50 -0500

    fixed pagination component, added pagination to blog page

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,6 @@
-import type {Route} from "./+types/index"
+import { useState } from "react";
+import Pagination from "~/components/Pagination";
+import type {Route} from "./+types/index";
 import type { PostMeta } from "~/types";
 import PostCard from "~/components/PostCard";
 export async function loader ({request}: Route.LoaderArgs): Promise<{posts: PostMeta[]}> {
@@ -8,6 +10,9 @@ export async function loader ({request}: Route.LoaderArgs): Promise<{posts: Post
     if (!res.ok) throw new Error('Failed to fetch data');
     
     const data = await res.json();
+    data.sort((a:PostMeta, b:PostMeta) =>{
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+    })
 
     return {posts:data};
 
@@ -16,16 +21,29 @@ export async function loader ({request}: Route.LoaderArgs): Promise<{posts: Post
 
 const BlogPage = ({loaderData}:Route.ComponentProps) => {
     const {posts} = loaderData; 
+    const [currentPage, setCurrentPage] = useState(1);
+    const postsPerPage = 3;
     
-    
-    
+    const totalPages = Math.ceil(posts.length / postsPerPage);
+    const indexOfLast = currentPage * postsPerPage;
+    const indexOfFirst = indexOfLast - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+
     return ( <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
         <h2 className="text-3xl text-white font-bold mb-8">
             Blog
         </h2>
-        {posts.map((post) =>(
+        {currentPosts.map((post) =>(
         <PostCard post={post} key={post.slug} />
         ))}
+        {
+            totalPages > 1 && (
+                <Pagination 
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={(page) => setCurrentPage(page)} />
+            )
+        }
     </div> );
 }
  
